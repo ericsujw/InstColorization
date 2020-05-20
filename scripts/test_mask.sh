ba2563d1f2c38e146a67337329065e72f93aacd5
ericsujw <w666666we@gmail.com>
2020-05-20 20:33:28 +0800

    clean repo

--- scripts/test_mask.sh
+++
@@ -1,5 +0,0 @@
-INPUT_DIR=example
-OUTPUT_DIR=results
-
-python inference_bbox.py --test_img_dir $INPUT_DIR
-python test_fusion.py --name test_fusion --sample_p 1.0 --model fusion --fineSize 256 --test_img_dir $INPUT_DIR --results_img_dir $OUTPUT_DIR
